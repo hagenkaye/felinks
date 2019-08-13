2bc58547c6be51d07879ace77bc5217d5dfa053a
Hagen Kaye <hagen.kaye@dejero.com>
2019-08-13 13:42:54 -0400

    add convenient script to configure minimal elinks build

---
+++ min-config.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./configure --disable-largefile --disable-bookmarks --disable-xbel --disable-sm-scripting --disable-cookies --disable-formhist --disable-globhist --disable-mailcap --disable-ipv6 --disable-ftp --disable-mouse --disable-leds --disable-sysmouse --enable-88-colors --enable-256-colors --enable-true-color --enable-cgi
+
